b2aa53c59a30fda9b7731493b2c369df38c7152e
lukaszlenart <lukasz.lenart@gmail.com>
2013-10-04 12:34:34 +0200

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kudos::Application.config.secret_key_base = '8d25bb5e22f7dc9ce433522f0c452fb7db58f5aaa0efe076edefc2e188c63e9969dd4934d9fbb98f8f6ff86ac03a07e870b6610f97e10770de3239d0c31d4b09'
